62d6dd6c29a8d71d610cee8d9822cdd1e5b43b01
Ben Barbour <ben.barbour@gmail.com>
2020-05-27 17:26:36 -0700

    Setup rust

---
+++ setup-rust.zsh
@@ -0,0 +1,19 @@
+if ! command -v rustup >/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output /tmp/sh.rustup.rs
+    chmod +x /tmp/sh.rustup.rs
+    /tmp/sh.rustup.rs -y --no-modify-path
+fi
+
+if [ ! -f "$ZDOTDIR/.zfunc/_rustup" ]; then
+    mkdir -p "$ZDOTDIR/.zfunc/"
+    rustup completions zsh > "$ZDOTDIR/.zfunc/_rustup"
+fi
+
+cargo install \
+    bat \
+    exa \
+    fd-find \
+    procs \
+    ripgrep \
+    skim \
+    ;
