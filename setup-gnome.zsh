bd73d85e565fe2148c6ee57a0e2c9d1a067f3683
Ben Barbour <ben.barbour@gmail.com>
2020-05-29 11:28:14 -0700

    Setup gnome

---
+++ setup-gnome.zsh
@@ -0,0 +1,13 @@
+gsettings set org.gnome.desktop.default-applications.terminal exec alacritty
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Alt>Up']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-down "@as []"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-up "@as []"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Shift><Super>Down']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Shift><Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Down']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Alt>Down']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>t']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>w']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>w']"
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
