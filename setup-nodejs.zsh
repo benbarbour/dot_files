7f9368a5b98ba2e3bf327e2e1025f73bffacd48f
Ben Barbour <ben.barbour@gmail.com>
2021-01-12 15:45:19 -0800

    Force install node and npx

--- setup-nodejs.zsh
+++ setup-nodejs.zsh
@@ -7,7 +7,7 @@ cd "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 source "$NVM_DIR/nvm.sh"
 
-nvm install node
+nvm install node --force
 nvm alias default node
 
 npm set init.author.name "Ben Barbour"
@@ -15,4 +15,4 @@ npm set init.author.email "ben.barbour@gmail.com"
 npm set init.license "ISC"
 npm set init.version "1.0.0"
 
-npm install -g npx
+npm install -g npx --force
