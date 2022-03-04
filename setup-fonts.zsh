13cd08970e51cd9be3b79d8ce285a0f1f24eae14
Ben Barbour <ben.barbour@gmail.com>
2022-03-04 12:19:52 -0800

    Fix setup-fonts.zsh

--- setup-fonts.zsh
+++ setup-fonts.zsh
@@ -1,4 +1,4 @@
 curl -L -o /tmp/nerdfont.zip \
     https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip /tmp/nerdfont.zip -o -d "$HOME/.local/share/fonts"
+unzip /tmp/nerdfont.zip -d "$HOME/.local/share/fonts"
 rm -f /tmp/nerdfont.zip
