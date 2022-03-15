63114105e7b0bc0af2c5c6d932a33cab623ca0e0
Ben Barbour <ben.barbour@gmail.com>
2022-03-15 15:04:20 -0700

    Neovim: lualine

--- xdg_config/nvim/lua/plugins.lua
+++ xdg_config/nvim/lua/plugins.lua
@@ -9,6 +9,17 @@ return require('packer').startup(function(use)
     vim.cmd('highlight clear LineNr')
   end}
 
+  use { 'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'solarized_dark'
+        }
+      }
+    end
+  }
+
 end)
 
 -- vim: set expandtab ts=2 sw=2:
