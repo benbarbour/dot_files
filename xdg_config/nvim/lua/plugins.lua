12a48308d1fc8413e70c1dfca87f74b2a1cf8e52
Ben Barbour <ben.barbour@gmail.com>
2022-03-15 14:37:15 -0700

    Neovim: use solarized colorscheme

--- xdg_config/nvim/lua/plugins.lua
+++ xdg_config/nvim/lua/plugins.lua
@@ -2,6 +2,13 @@ return require('packer').startup(function(use)
 
   use { 'wbthomason/packer.nvim' }  -- Packer can manage itself
 
+  use { 'Iron-E/nvim-soluarized', config = function()
+    vim.o.termguicolors = true
+    vim.o.background = 'dark'
+    vim.cmd('colorscheme soluarized')
+    vim.cmd('highlight clear LineNr')
+  end}
+
 end)
 
 -- vim: set expandtab ts=2 sw=2:
