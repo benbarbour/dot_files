b6a7a1a3a81eeb777bf90bb1221d768f7b6f1460
Ben Barbour <ben.barbour@ge.com>
2023-03-03 11:31:56 -0800

    Fix nodejs config for newer versions

--- setup-nodejs.zsh
+++ setup-nodejs.zsh
@@ -9,9 +9,9 @@ set -v
 nvm install --lts
 nvm alias default node
 
-npm set init-author=name="Ben Barbour"
-npm set init-author-email="ben.barbour@gmail.com"
-npm set init-license="ISC"
-npm set init-version="1.0.0"
+npm config set init-author-name="Ben Barbour"
+npm config set init-author-email="ben.barbour@gmail.com"
+npm config set init-license="ISC"
+npm config set init-version="1.0.0"
 
 command -v npx || npm install -g npx
