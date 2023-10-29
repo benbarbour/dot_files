4938ac3379e7227be4dc7089ad34b2f683cb104f
Ben Barbour <ben.barbour@gmail.com>
2023-10-29 10:46:23 -0700

    nvim: add treesitter parsers

---
+++ nvchad/plugins/nvim-treesitter.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      -- defaults
+      "vim",
+      "lua",
+
+      -- web
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "json",
+
+      -- misc things I used
+      "bash",
+      "bitbake",
+      "c",
+      "cmake",
+      "comment",
+      "csv",
+      "diff",
+      "dockerfile",
+      "git_config",
+      "git_rebase",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "python",
+      "requirements",
+      "rust",
+      "ssh_config",
+      "toml",
+      "yaml",
+    }
+  },
+}
