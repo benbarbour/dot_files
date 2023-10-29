1dd5f2254d2095cf043e1c8c24489b9cb2d4be3f
Ben Barbour <ben.barbour@gmail.com>
2023-10-29 10:21:24 -0700

    NvChad comfort tweaks

--- nvchad/chadrc.lua
+++ nvchad/chadrc.lua
@@ -1,3 +1,12 @@
-local M = {}
+local M = {
+  plugins = "custom.plugins",
+  mappings = require "custom.mappings",
+  ui = {
+    cheatsheet = { theme = "simple" },
+    theme = "solarized_dark",
+    tabufline = { enabled = false },
+    telescope = { style = "bordered" },
+  }
+}
 
 return M
--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,3 +1,12 @@
-local M = {}
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-return M
+-- show these special characters as these symbols
+vim.opt.list = true
+vim.opt.listchars = {
+  extends = '…',
+  nbsp = '␣',
+  precedes = '…',
+  tab = '↦ ',
+  trail = '·',
+}
