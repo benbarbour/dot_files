0e739118fdb26059db54c31f68cc00d797774ec7
Ben Barbour <ben.barbour@gmail.com>
2023-10-29 14:25:00 -0700

    nvim: autoformat on save

--- nvchad/configs/null-ls.lua
+++ nvchad/configs/null-ls.lua
@@ -8,8 +8,23 @@ return function()
     b.formatting.stylua,
   }
 
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
   null_ls.setup({
     -- debug = true,
     sources = sources,
+
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ async = false })
+          end,
+        })
+      end
+    end,
   })
 end
