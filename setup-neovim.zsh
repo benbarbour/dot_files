ead924415e93bcc12ad616bac298be6dffcfaac4
Ben Barbour <ben.barbour@gmail.com>
2023-10-28 20:52:06 -0700

    Switch to NvChad

--- setup-neovim.zsh
+++ setup-neovim.zsh
@@ -19,21 +19,14 @@ latest_ver=$(curl -sS -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/neovim/neovim/releases/latest | jq -r .tag_name)
 
 if [ ! hash nvim &> /dev/null ] || [ "$(nvim --version | head -n 1 | cut -d' ' -f2)" != "$latest_ver" ]; then
+    sudo rm -rf "$NVIM_PATH"
     sudo curl -sS --create-dirs -L -o "$NVIM_PATH" \
         https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
     sudo chmod +x "$NVIM_PATH"
     sudo ln -sf "$NVIM_PATH" "${NVIM_PATH%.appimage}"
 fi
 
-PACKER_DIR="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
-
-if [ ! -d "$PACKER_DIR" ]; then
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim "$PACKER_DIR"
-else
-    git -C "$PACKER_DIR" fetch --depth 1
-    git -C "$PACKER_DIR" reset --hard origin/master
-    git -C "$PACKER_DIR" clean -dfx
+if [ ! -d ~/.config/nvim ]; then
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+  ln -s ~/.bbenv/nvchad ~/.config/nvim/lua/custom
 fi
-
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'UpdateRemotePlugins' -c 'qall'
--- xdg_config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require('settings')
-require('keys')
-require('plugins')
