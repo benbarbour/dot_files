1453cf2dbdda8183341b14f0f6d44439eddbfb4f
Ben Barbour <ben.barbour@gmail.com>
2023-10-28 20:46:35 -0700

    Update font

--- setup-fonts.zsh
+++ setup-fonts.zsh
@@ -1,7 +1,7 @@
 header "SETUP FONTS"
 if [ ! -f "$HOME/.local/share/fonts/Fira Code Regular Nerd Font Complete.otf" ] ; then
     curl -sS -L -o /tmp/nerdfont.zip \
-        https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+        https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
     unzip /tmp/nerdfont.zip -d "$HOME/.local/share/fonts"
     rm -f /tmp/nerdfont.zip
 fi
