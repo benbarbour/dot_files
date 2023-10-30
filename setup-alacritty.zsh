3e0252b46c252405ac678f060d2b27e8a6fc8c71
Ben Barbour <ben.barbour@ge.com>
2023-10-30 08:58:18 -0700

    build alacritty

--- setup-alacritty.zsh
+++ setup-alacritty.zsh
@@ -1,5 +1,32 @@
 header "SETUP ALACRITTY"
 
-[ find /etc/apt/ -name '*.list' | xargs cat | grep '^[[:space:]]*deb' | grep -q 'mmstick76/alacritty' ] && sudo add-apt-repository -y ppa:mmstick76/alacritty
+sudo apt install \
+    cmake \
+    pkg-config \
+    libfreetype6-dev \
+    libfontconfig1-dev \
+    libxcb-xfixes0-dev \
+    libxkbcommon-dev \
+    python3
 
-sudo apt-get -y install alacritty
+pushd $HOME/src
+if [ -d alacritty ]; then
+    cd alacritty
+    git pull
+else
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+fi
+
+cargo build --release && ln -f -s /home/ben/src/alacritty/target/release/alacritty /home/ben/bin/alacritty
+
+echo """[Desktop Entry]
+Name=Alacritty
+Exec=/home/ben/src/alacritty/target/release/alacritty
+Terminal=false
+Icon=/home/ben/src/alacritty/alacritty/windows/alacritty.ico
+Type=Application
+Comment=Terminal written in rust
+""" > $HOME/.local/share/applications/alacritty.desktop
+
+popd
