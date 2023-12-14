07874d28a4a4c1423b6cf0ae28cba6e965d2a2f3
Ben Barbour <ben.barbour@gmail.com>
2023-12-14 11:42:44 -0800

    nvim: added typescript-tools

---
+++ nvchad/plugins/typescript-tools.lua
@@ -0,0 +1,7 @@
+print("BEN1")
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  ft = "typescript",
+  -- setup called in `configs.lspconfig`
+}
