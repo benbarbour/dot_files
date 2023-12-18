cdf2b6ad4369c4746f261d6db47a2eb197c7e1fe
Ben Barbour <ben.barbour@gmail.com>
2023-12-18 10:55:10 -0800

    nvim: renable typescript-tools; keep biome

--- nvchad/configs/lspconfig.lua
+++ nvchad/configs/lspconfig.lua
@@ -21,4 +21,8 @@ lspconfig.pyright.setup({
   filetypes = { "python" },
 })
 
+require("typescript-tools").setup({
+  on_attach = on_attach,
+})
+
 vim.diagnostic.config({ virtual_text = false })
---
+++ nvchad/plugins/typescript-tools.lua
@@ -0,0 +1,6 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  ft = "typescript",
+  -- setup called in `configs.lspconfig`
+}
