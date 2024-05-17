87de8b40fbe9e44d5b950335e157c401d4f11a2e
Ben Barbour <ben.barbour@ge.com>
2024-05-17 13:51:21 -0700

    fix setup-go.zsh

--- setup-go.zsh
+++ setup-go.zsh
@@ -2,14 +2,14 @@ header "SETUP GO"
 
 update_needed=0
 latest_ver=$(curl -sS 'https://go.dev/VERSION?m=text' | head -n 1)
-echo "Go lastest version is: $lastest_ver"
 
 if ! command -v go >/dev/null; then
+    echo "Go not installed - update needed"
     update_needed=1
 else
     current_ver=$(go version | cut -d' ' -f3)
-    echo "Go current version is: $current_ver"
     if [[ "$current_ver" != "$latest_ver" ]]; then
+        echo "Go current version is $current_ver but latest version is $latest_ver - update needed"
         update_needed=1
     fi
 fi
@@ -19,3 +19,5 @@ if [[ "$update_needed" == 1 ]]; then
     curl -sS -L "https://go.dev/dl/$latest_ver.linux-amd64.tar.gz" -o /tmp/go.tar.gz
     sudo tar -C /usr/local -xzf /tmp/go.tar.gz
 fi
+
+echo "Go installed and up to date!"
