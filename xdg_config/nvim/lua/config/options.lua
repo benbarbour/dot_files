193782f902b32dc8df2d1a5a740ca978d508490a
Ben Barbour <ben.barbour@ge.com>
2024-05-17 19:41:52 -0700

    nvim: set listchars the way I like it

--- xdg_config/nvim/lua/config/options.lua
+++ xdg_config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- show these special characters as these symbols
+vim.opt.list = true
+vim.opt.listchars = {
+  extends = "…",
+  nbsp = "␣",
+  precedes = "…",
+  tab = "↦ ",
+  trail = "·",
+}
