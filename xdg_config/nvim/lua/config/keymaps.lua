1bf0851b536f3147667a9687c0ed38dcf71c2ca2
Ben Barbour <ben.barbour@ge.com>
2024-05-17 19:41:46 -0700

    nvim: added null register shortcuts

--- xdg_config/nvim/lua/config/keymaps.lua
+++ xdg_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<A-d>", '"_d', { desc = "Delete to null register" })
+map("n", "x", '"_x', { desc = "Delete to null register", remap = false })
+map("n", "X", '"_x', { desc = "Delete to null register", remap = false })
