8b3f2e96a67b471cdc18e2f2b6bd1451bed7da8e
Ben Barbour <ben.barbour@gmail.com>
2024-07-31 14:56:24 -0700

    Fix python

---
+++ setup-python.zsh
@@ -0,0 +1,23 @@
+py_ver=3.11
+header "INSTALL PYTHON $py_ver"
+if ! command -v "python$py_ver"; then
+	sudo add-apt-repository -y ppa:deadsnakes/ppa
+	sudo apt-get -y update
+	sudo apt-get -y install python$py_ver python$py_ver-dev
+fi
+
+header "INSTALL PIPX"
+# see https://github.com/pypa/pipx/issues/1481
+if ! command -v pipx; then
+	sudo apt-get -y install pipx                   # installs pipx to /usr/local/bin
+	pipx ensurepath                                # adds ~/.local/bin to path
+	pipx install pipx                              # installs latest pipx to ~/.local/bin
+	sudo apt-get purge -y --autoremove pipx        # remove pipx via apt, and all extra files including autocomplete from /usr/local/bin
+	hash -r                                        # pick up the new path
+	sudo ~/.local/bin/pipx install --global pipx   # installs latest pipx version to /usr/local/bin because --global works.
+	pipx uninstall pipx                            # remove the local version
+	pipx ensurepath                                # make sure the ~/.local/bin is added to $PATH
+	sudo pipx --global ensurepath                  # make sure that /usr/local/bin is set to global install
+else
+	sudo pipx upgrade --global pipx
+fi
