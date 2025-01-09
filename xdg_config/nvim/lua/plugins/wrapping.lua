eb660d3cbeb245f2c3df457a4a2a8bb414fbb4f1
Ben Barbour <ben.barbour@gmail.com>
2025-01-09 09:50:48 -0800

    nvim: disable wrapping.lua notifications

--- xdg_config/nvim/lua/plugins/wrapping.lua
+++ xdg_config/nvim/lua/plugins/wrapping.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     create_commands = true,
     create_keymaps = false,
-    notify_on_switch = true,
+    notify_on_switch = false,
     softener = { markdown = true },
   },
 }
