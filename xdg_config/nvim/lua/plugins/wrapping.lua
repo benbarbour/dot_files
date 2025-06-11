22698b512176e24e2b7770db26e420523d5d910b
Ben Barbour <ben.barbour@gmail.com>
2025-06-11 10:00:38 -0700

    nvim: remove wrapping plugin

--- xdg_config/nvim/lua/plugins/wrapping.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "andrewferrier/wrapping.nvim",
-  opts = {
-    create_commands = true,
-    create_keymaps = false,
-    notify_on_switch = false,
-    softener = { markdown = true },
-  },
-}
