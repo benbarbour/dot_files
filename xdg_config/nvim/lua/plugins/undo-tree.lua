c01e93600e8dd3afe3abc6f69822ec0c636687b8
Ben Barbour <ben.barbour@gmail.com>
2025-06-17 10:50:06 -0700

    nvim: undo tree plugin

---
+++ xdg_config/nvim/lua/plugins/undo-tree.lua
@@ -0,0 +1,8 @@
+return {
+  "jiaoshijie/undotree",
+  dependencies = "nvim-lua/plenary.nvim",
+  config = true,
+  keys = {
+    { "<leader>i", "<cmd>lua require('undotree').toggle()<cr>", "undotree" },
+  },
+}
